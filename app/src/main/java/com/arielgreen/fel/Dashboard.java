eae161c7ca7f929a75b4a5875be941f8a16d2af0
pankajfel <pankaj@thenobi.in>
2020-09-24 11:17:01 +0530

    newInit

---
+++ app/src/main/java/com/arielgreen/fel/Dashboard.java
@@ -0,0 +1,26 @@
+package com.arielgreen.fel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class Dashboard extends AppCompatActivity {
+
+    TextView text;
+    Bundle bundle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        bundle = getIntent().getExtras();
+        text = findViewById(R.id.text);
+        text.setText("Hi, "+bundle.getString("user"));
+
+        Snackbar.make(findViewById(R.id.root), "Welcome!!", Snackbar.LENGTH_LONG).show();
+    }
+}
---
+++ app/src/main/java/com/arielgreen/fel/SplashScreen.java
@@ -0,0 +1,32 @@
+package com.arielgreen.fel;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static int time = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+
+            }
+        },time);
+    }
+
+}
